9e42010b605bae92d193606f24b8e451b281190b
Nicolas Jourdain <nicolas.jourdain2@gmail.com>
2025-10-07 13:14:27 -0400

    Fix merge

--- projects/integration/src/app/api-tests/signal-store.spec.ts
+++ projects/integration/src/app/api-tests/signal-store.spec.ts
@@ -1,4 +1,4 @@
-import { staticTest } from '@s-libs/ng-dev';
+import { staticTest } from '@sama/ng-dev';
 import {
   PersistentStore,
   pushToArrayStore,
--- projects/micro-dash/src/lib/util/flow.spec.ts
+++ projects/micro-dash/src/lib/util/flow.spec.ts
@@ -1,4 +1,4 @@
-import { staticTest } from '@s-libs/ng-dev';
+import { staticTest } from '@sama/ng-dev';
 import { expectTypeOf } from 'expect-type';
 import { curry, head, identity } from 'lodash';
 import { flow } from './flow';
